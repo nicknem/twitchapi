d02f67c207da50e56bb049b820bf8c83be224540
nicknem <antonin.archer@hec.edu>
2016-05-24 22:50:47 +0100

    Add API call to Twitch API

--- source/javascripts/twitch_handler.js
+++ source/javascripts/twitch_handler.js
@@ -1,3 +1,12 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document ).ready(function() {
+  // Returns a stream object if FreeCodeCamp is streaming
+  $.getJSON('https://api.twitch.tv/kraken/streams/freecodecamp?callback=?', function(data) {
+    // if stream object null say nobody is streaming
+    // if people are streaming
+    console.log(data);
+  });
 });
+
+
+var streamers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp",
+                 "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
